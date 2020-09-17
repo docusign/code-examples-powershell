7c7ca725b2abfa8ef9e0c535916ac367702c4acb
Serhii Ovsianikov <serhii.ovsianikov@sigma.software>
2020-09-18 00:07:47 +0300

    Updated examples/eg001EmbeddedSigning.ps1

--- examples/eg001EmbeddedSigning.ps1
+++ examples/eg001EmbeddedSigning.ps1
@@ -1,23 +1,12 @@
 # Embedded signing ceremony
 
-# If settings.txt file exist, we use all variables from this file
-if (Test-Path .\config\settings.txt) {
-    Get-Content ".\config\settings.txt" | Foreach-Object {
-        $var = $_.Split('=')
-        if ($var.Lengh -ne 2 -and $var[0].IsNullOrEmpty) {
-            throw;
-        }
-        else {
-            New-Variable -Name $var[0] -Value $var[1] -Force -Scope Global
-        }
-    }
-}
+# Ger required environment variables from .\config\settings.json file
+$variables = Get-Content .\config\settings.json -Raw | ConvertFrom-Json
 
-# Get environment variables
-$CC_EMAIL = $(Get-Variable CC_EMAIL -ValueOnly) -replace '["]'
-$CC_NAME = $(Get-Variable CC_NAME -ValueOnly) -replace '["]'
-$SIGNER_EMAIL = $(Get-Variable SIGNER_EMAIL -ValueOnly) -replace '["]'
-$SIGNER_NAME = $(Get-Variable SIGNER_NAME -ValueOnly) -replace '["]'
+$CC_EMAIL = [System.Environment]::ExpandEnvironmentVariables($variables.CC_EMAIL)
+$CC_NAME = [System.Environment]::ExpandEnvironmentVariables($variables.CC_NAME)
+$SIGNER_EMAIL = [System.Environment]::ExpandEnvironmentVariables($variables.SIGNER_EMAIL)
+$SIGNER_NAME = [System.Environment]::ExpandEnvironmentVariables($variables.SIGNER_NAME)
 
 # Configuration
 # 1. Search for and update '{USER_EMAIL}' and '{USER_FULLNAME}'.
@@ -41,11 +30,10 @@ $accountID = Get-Content .\config\API_ACCOUNT_ID
 #  recipient 2 - cc
 #  The envelope will be sent first to the signer.
 #  After it is signed, a copy is sent to the cc person.
-$basePath = "https://demo.docusign.net/restapi"
+$apiUri = "https://demo.docusign.net/restapi"
 
 # temp files:
 $requestData = New-TemporaryFile
-$requestDataTemp = New-TemporaryFile
 $response = New-TemporaryFile
 $doc1Base64 = New-TemporaryFile
 
@@ -56,73 +44,59 @@ $demoFile = $(Get-Location).Path + '\demo_documents\World_Wide_Corp_lorem.pdf'
 Write-Output "`nSending the envelope request to DocuSign...`n"
 
 # Concatenate the different parts of the request
-Write-Output '{
-    "emailSubject": "Please sign this document set",
-    "documents": [
-        {
-            "documentBase64": "doc1Base64",
-            "name": "Lorem Ipsum",
-            "fileExtension": "pdf",
-            "documentId": "1"
-        }
-    ],
-    "recipients": {
-        "carbonCopies": [
-            {
-                "email": "CC_EMAIL",
-                "name": "CC_NAME",
-                "recipientId": "2",
-                "routingOrder": "2"
-            }
-        ],
-        "signers": [
-            {
-                "email": "SIGNER_EMAIL",
-                "name": "SIGNER_NAME",
-                "recipientId": "1",
-                "routingOrder": "1",
-                "clientUserId": "1000",
-                "tabs": {
-                    "signHereTabs": [
-                        {
-                            "anchorString": "/sn1/",
-                            "anchorUnits": "pixels",
-                            "anchorXOffset": "20",
-                            "anchorYOffset": "10"
-                        }
-                    ]
-                }
-            }
-        ]
-    },
-    "status": "sent"
-}' > $requestDataTemp
+@{
+    emailSubject = "Please sign this document set";
+    documents    = @(
+        @{
+            documentBase64 = "$(Get-Content $doc1Base64)";
+            name           = "Lorem Ipsum";
+            fileExtension  = "pdf";
+            documentId     = "1";
+        };
+    );
+    recipients   = @{
+        carbonCopies = @(
+            @{
+                email        = $CC_EMAIL;
+                name         = $CC_NAME;
+                recipientId  = "2";
+                routingOrder = "2";
+            };
+        );
+        signers      = @(
+            @{
+                email        = $SIGNER_EMAIL;
+                name         = $SIGNER_NAME;
+                recipientId  = "1";
+                routingOrder = "1";
+                clientUserId = "1000";
+                tabs         = @{
+                    signHereTabs = @(
+                        @{
+                            anchorString  = "/sn1/";
+                            anchorUnits   = "pixels";
+                            anchorXOffset = "20";
+                            anchorYOffset = "10";
+                        };
+                    );
+                };
+            };
+        );
+    };
+    status       = "sent";
+} | ConvertTo-Json -Depth 32 > $requestData
 
-$((Get-Content -path $requestDataTemp -Raw) `
-        -replace 'doc1Base64', $(Get-Content $doc1Base64) `
-        -replace 'CC_EMAIL', $CC_EMAIL `
-        -replace 'CC_NAME', $CC_NAME `
-        -replace 'SIGNER_EMAIL', $SIGNER_EMAIL `
-        -replace 'SIGNER_NAME', $SIGNER_NAME ) > $requestData
+Invoke-RestMethod `
+    -Uri "${apiUri}/v2.1/accounts/${accountId}/envelopes" `
+    -Method 'POST' `
+    -Headers @{
+    'Authorization' = "Bearer $accessToken";
+    'Content-Type'  = "application/json";
+} `
+    -InFile (Resolve-Path $requestData).Path `
+    -OutFile $response
 
-$headers = @{
-    'Authorization' = "Bearer $accessToken"
-    'Content-Type'  = 'application/json'
-}
-
-$parameters = @{
-    Uri    = $basePath + "/v2.1/accounts/" + $accountId + "/envelopes"
-    Method = 'POST'
-    Infile = $requestData
-}
-
-try {
-    Invoke-RestMethod -Headers $headers @parameters -OutFile $response
-    Write-Output "Response: $(Get-Content -Raw $response)`n"
-}
-catch {
-    Write-Error $_
-}
+Write-Output "Response: $(Get-Content -Raw $response)`n"
 
 # pull out the envelopeId
 $envelopeId = $(Get-Content $response | ConvertFrom-Json).envelopeId
@@ -146,25 +120,17 @@ $json = [ordered]@{
     'clientUserId'         = 1000
 } | ConvertTo-Json -Compress
 
-$headers = @{
-    'Authorization' = "Bearer $accessToken"
-    'Content-Type'  = 'application/json'
-}
+Invoke-RestMethod `
+    -Uri "${apiUri}/v2.1/accounts/${accountId}/envelopes/${envelopeId}/views/recipient" `
+    -Method 'POST' `
+    -Headers @{
+    'Authorization' = "Bearer $accessToken";
+    'Content-Type'  = "application/json";
+} `
+    -Body $json `
+    -OutFile $response
 
-$parameters = @{
-    Uri    = $basePath + "/v2.1/accounts/" + $accountId + "/envelopes/" + $envelopeId + "/views/recipient"
-    Method = 'POST'
-    Body   = $json
-}
-
-try {
-    Invoke-RestMethod -Headers $headers @parameters -OutFile $response
-    Write-Output "Response: $(Get-Content -Raw $response)`n"
-}
-catch {
-    Write-Error $_
-}
-
+Write-Output "Response: $(Get-Content -Raw $response)`n"
 $signingCeremonyUrl = $(Get-Content $response | ConvertFrom-Json).url
 
 # ***DS.snippet.0.end
@@ -176,7 +142,6 @@ Start-Process $signingCeremonyUrl
 
 # cleanup
 Remove-Item $requestData
-Remove-Item $requestDataTemp
 Remove-Item $response
 Remove-Item $doc1Base64
 

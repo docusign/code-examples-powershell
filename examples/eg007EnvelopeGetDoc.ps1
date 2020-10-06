8f853499edf5e40c0b636ec5b70102c3d22bd4ad
Serhii Ovsianikov <serhii.ovsianikov@sigma.software>
2020-10-07 00:10:01 +0300

    Fixed menu in eg007EnvelopeGetDoc.ps1

--- examples/eg007EnvelopeGetDoc.ps1
+++ examples/eg007EnvelopeGetDoc.ps1
@@ -30,68 +30,78 @@ else {
 $docChoice = "1"
 $outputFileExtension = "pdf"
 
-
-# Create a List with Documents
-$listDocs = @(
-    "Document 1",
-    "Document 2",
-    "Document 3",
-    "Certificate of Completion",
-    "Documents combined together",
-    "ZIP file"
-)
-
-# Create a blank array to hold menu
-$formattedListDocs = @()
-# Even Odd Columns
-for ($i = 0; $i -lt $listDocs.Count; $i += 1) {
-    if ($null -ne $listDocs[$i + 1]) {
-        $formattedListDocs += [PSCustomObject]@{
-            Odd = "$($i+1)) $($listDocs[$i])";
-        }
-    }
-    else {
-        $formattedListDocs += [PSCustomObject]@{
-            Odd  = "$($i+1)) $($listDocs[$i])";
-            Even = ""
-        }
-    }
+function sendDocuments {
+    Write-Output "Sending the EnvelopeDocuments::get request to DocuSign..."
+    # ***DS.snippet.0.start
+    Invoke-RestMethod `
+        -Uri "${apiUri}/v2.1/accounts/${accountId}/envelopes/${envelopeId}/documents/${docChoice}" `
+        -Method 'GET' `
+        -Headers @{
+        'Authorization' = "Bearer $accessToken";
+        'Content-Type'  = "application/json";
+    } `
+        -OutFile ${outputFile}${outputFileExtension}
+    # ***DS.snippet.0.end
+    Write-Output "The document(s) are stored in file ${outputFile}${outputFileExtension}"
+    Write-Output "Done."
 }
 
-# Output menu
-$formattedListDocs | Format-Table -HideTableHeaders
+do {
+    # Create a List with Documents
+    $listDocs = @(
+        "Document 1",
+        "Document 2",
+        "Document 3",
+        "Certificate of Completion",
+        "Documents combined together",
+        "ZIP file",
+        "Exit"
+    )
 
-# Read method from console
-$METHOD = Read-Host "Select a document or document set to download"
-switch ($METHOD) {
-    '1' {
-        $docChoice = "1"
-    } '2' {
-        $docChoice = "2"
-    } '3' {
-        $docChoice = "3"
-    } '4' {
-        $docChoice = "certificate"
-    } '5' {
-        $docChoice = "combined"
-    } '6' {
-        $docChoice = "archive"
-        $outputFileExtension = "zip"
+    # Create a blank array to hold menu
+    $formattedListDocs = @()
+    # Even Odd Columns
+    for ($i = 0; $i -lt $listDocs.Count; $i += 1) {
+        if ($null -ne $listDocs[$i + 1]) {
+            $formattedListDocs += [PSCustomObject]@{
+                Odd = "$($i+1)) $($listDocs[$i])";
+            }
+        }
+        else {
+            $formattedListDocs += [PSCustomObject]@{
+                Odd  = "$($i+1)) $($listDocs[$i])";
+                Even = ""
+            }
+        }
     }
-}
 
-Write-Output "Sending the EnvelopeDocuments::get request to DocuSign..."
+    # Output menu
+    $formattedListDocs | Format-Table -HideTableHeaders
 
-# ***DS.snippet.0.start
-Invoke-RestMethod `
-    -Uri "${apiUri}/v2.1/accounts/${accountId}/envelopes/${envelopeId}/documents/${docChoice}" `
-    -Method 'GET' `
-    -Headers @{
-    'Authorization' = "Bearer $accessToken";
-    'Content-Type'  = "application/json";
-} `
-    -OutFile ${outputFile}${outputFileExtension}
-# ***DS.snippet.0.end
-
-Write-Output "The document(s) are stored in file ${outputFile}${outputFileExtension}"
-Write-Output "Done."
+    # Read method from console
+    $METHOD = Read-Host "Select a document or document set to download"
+    switch ($METHOD) {
+        '1' {
+            $docChoice = "1"
+            sendDocuments
+        } '2' {
+            $docChoice = "2"
+            sendDocuments
+        } '3' {
+            $docChoice = "3"
+            sendDocuments
+        } '4' {
+            $docChoice = "certificate"
+            sendDocuments
+        } '5' {
+            $docChoice = "combined"
+            sendDocuments
+        } '6' {
+            $docChoice = "archive"
+            $outputFileExtension = "zip"
+            sendDocuments
+        } '7' {
+            exit 0
+        }
+    }
+} until ($METHOD -eq '7')

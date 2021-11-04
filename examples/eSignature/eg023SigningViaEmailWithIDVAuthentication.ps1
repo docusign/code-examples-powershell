07c421654c7a79350edfdd21d3a5dd233dcccd68
Inbar Gazit <inbar.gazit@docusign.com>
2021-11-04 15:30:15 -0700

    Updating both examples 20 (Phone Auth) and 23 (IDV) to correctly find the workflowID as needed  (DEVDOCS-6118 and DEVDOCS-6127)

--- examples/eSignature/eg023SigningViaEmailWithIDVAuthentication.ps1
+++ examples/eSignature/eg023SigningViaEmailWithIDVAuthentication.ps1
@@ -26,38 +26,31 @@ $docBase64 = New-TemporaryFile
 [Convert]::ToBase64String([System.IO.File]::ReadAllBytes((Resolve-Path ".\demo_documents\World_Wide_Corp_lorem.pdf"))) > $docBase64
 
 # - Construct your API headers
+# Step 2 start
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.add("Authorization", "Bearer $oAuthAccessToken")
 $headers.add("Accept", "application/json, text/plain, */*")
 $headers.add("Content-Type", "application/json;charset=UTF-8")
 $headers.add("Accept-Encoding", "gzip, deflate, br")
 $headers.add("Accept-Language", "en-US,en;q=0.9")
+# Step 2 end
 
 # - Obtain your workflow ID
+# Step 3 start
 $uri = "https://demo.docusign.net/restapi/v2.1/accounts/$APIAccountId/identity_verification"
 
 Write-Output "Attempting to retrieve your account's workflow ID"
 
-try {
-	Write-Output "Response:"
-	$result = Invoke-RestMethod -uri $uri -headers $headers -method GET
-	$result.content
-	#Obtain the workflow ID from the API response
-	$workflowId = $result.identityVerification.workflowId
-}
-catch {
-	$int = 0
-	foreach ($header in $_.Exception.Response.Headers) {
-		#On error, display the error, the line that triggered the error, and the TraceToken
-		if ($header -eq "X-DocuSign-TraceToken") { Write-Output "TraceToken : " $_.Exception.Response.Headers[$int] }
-		$int++
-	}
-	Write-Output "Error : "$_.ErrorDetails.Message
-	Write-Output "Command : "$_.InvocationInfo.Line
-}
+Write-Output "Response:"
+$result = Invoke-RestMethod -uri $uri -headers $headers -method GET
+$result.content
+#Obtain the workflow ID from the API response
+$workflowId = [System.Linq.Enumerable]::FirstOrDefault($result.identityVerification, [func[object, bool]] { param($x) $x.defaultName -eq "DocuSign ID Verification"}).workflowId
+# Step 3 end
 
 # - Construct your envelope JSON body
 # Note: If you did not successfully obtain your workflow ID, this step will fail.
+# Step 4 start
 $body = @"
 {
 	"documents": [{
@@ -82,8 +75,8 @@ $body = @"
 						"pageNumber": "1",
 						"recipientId":"1",
 						"tabLabel": "SignHereTab",
-						"xPosition": "75",
-						"yPosition": "572"
+						"xPosition": "200",
+						"yPosition": "160"
 					}]
 				},
 			"templateAccessCodeRequired": null,
@@ -100,12 +93,14 @@ $body = @"
 	"status": "Sent"
 }
 "@
+# Step 4 end
 Write-Output ""
 Write-Output "Request: "
 Write-Output $body
 
 # a) Make a POST call to the createEnvelopes endpoint to create a new envelope.
 # b) Display the JSON structure of the created envelope
+# Step 5 start
 $uri = "https://demo.docusign.net/restapi/v2.1/accounts/$APIAccountId/envelopes"
 try {
 	Write-Output "Response:"
@@ -121,6 +116,7 @@ catch {
 	Write-Output "Error : "$_.ErrorDetails.Message
 	Write-Output "Command : "$_.InvocationInfo.Line
 }
+# Step 5 end
 
 # cleanup
 Remove-Item $docBase64

f5ae78a07a2bea36227891c49aa100b9c5057535
Inbar Gazit <inbar.gazit@docusign.com>
2023-06-05 14:53:21 -0700

    DEVDOCS-10580 - adding codeDepot markers

--- examples/eSignature/eg015EnvelopeTabData.ps1
+++ examples/eSignature/eg015EnvelopeTabData.ps1
@@ -20,18 +20,22 @@ else {
 }
 
 # Step 2. Create your authorization headers
+#ds-snippet-start:eSign15Step2
 $headers = @{
   'Authorization' = "Bearer $accessToken";
   'Accept'        = "application/json";
   'Content-Type'  = "application/json";
 }
+#ds-snippet-end:eSign15Step2
 
 # Step 3. a) Make a GET call to the form_data endpoint to retrieve your envelope tab values
 #         b) Display the JSON response
+#ds-snippet-start:eSign15Step3
 $result = $(Invoke-WebRequest `
     -Uri "${apiUri}/v2.1/accounts/${accountId}/envelopes/${envelopeId}/form_data" `
     -Method 'GET' `
     -Headers $headers)
+#ds-snippet-end:eSign15Step3
 
 if ( $result.StatusCode -gt 201) {
   Write-Output "Retrieving envelope form data has failed."
